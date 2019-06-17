38840ba95a923cd77a17093c4c871e2178a453ab
Ntsako SELAULE <nselaule@c4r14s1.wethinkcode.co.za>
2019-06-17 18:24:12 +0200

    starting

---
+++ src /simulator/vehicle/Aircraft.java
@@ -0,0 +1,17 @@
+public class Aircraft {
+    protected long id;
+    protected string name;
+    // protected Coor
+    private long idCounter;
+
+    protected Aircraft (String _name) {
+        name = _name;
+    }
+
+    private long nextId() {
+
+    }
+    public static void main(String[] argv) {
+        System.out.println("HI there dummie");
+    }
+}
---
+++ src /weather/Coordinates.java
@@ -0,0 +1,20 @@
+package Coordinates {
+
+public class Coordinates {
+    private int longitude;
+    private int latitude;
+    private int height;
+
+    public getLongitude() {
+        return longitude;
+	}
+	
+	public getLatitude() {
+        return latitude;
+	}
+	
+	public getHeight() {
+        return height;
+	}
+	}
+}
